9f359090c37128556ab8c042040e46b0d163ae87
Raphael Borba <raphaelbsx@gmail.com>
2022-08-24 23:40:31 -0300

    Funções adicionadas (startWeb, askName, showMessages and loadMessages)

---
+++ script.js
@@ -0,0 +1,78 @@
+
+
+const startWeb = (dado)=>{
+    
+    let name = askName()
+    let arrayNames = dado.data
+
+    while(true){
+
+        let deny=0
+
+        for(let i = 0; i<arrayNames.length; i++){
+        
+            if(name === arrayNames[i].name || name.length ===0){
+        
+                alert('Nome repetido')
+        
+                name = askName()
+                deny=1
+            }
+        }
+        if(deny ===0){
+            break;
+        }
+    }
+    const promesse = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants ',{
+        name: name
+    })
+
+    /* CHECKS IF THE USER KEEPS ONLINE EVERY 5 SEG */
+    setInterval(()=>{
+        axios.post('https://mock-api.driven.com.br/api/v6/uol/status',{
+        name: name
+    })
+    }, 5000)
+}
+
+const askName = ()=>{
+    let name = prompt('Digite seu nome:')
+    return name
+}
+
+/* START THE SITE */
+
+const promessa  = axios.get('https://mock-api.driven.com.br/api/v6/uol/participants')
+promessa.then(startWeb)
+
+const showMessages =(dado)=>{
+    let data = dado.data
+    let ul = document.querySelector('ul')
+
+    for(let i =0; i<data.length;i++){
+
+        if(data[i].type === 'status'){
+
+            ul.innerHTML+= `
+
+            <li class="enter-leave-group">
+                <span><span class="time">(${data[i].time})</span> <span class="name"> ${data[i].from} </span> ${data[i].text}</span>
+            </li>
+`
+        }else if(data[i].type === 'message'){
+            ul.innerHTML+=`
+           
+            <li class="msg-everybody">
+                <span><span class="time">(${data[i].time})</span> <span class="name"> ${data[i].from} </span>para <span class="name">${data[i].to}:</span> ${data[i].text}</span>
+            </li>`
+        }
+    }
+}
+
+const loadMessages = ()=>{
+    const promesse = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages')
+    promesse.then(showMessages)
+
+}
+loadMessages()
+
